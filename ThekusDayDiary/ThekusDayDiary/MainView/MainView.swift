cf497696995910c330adf006477532748078a26e
direchan <hdjin92@gmail.com>
2023-08-20 07:00:17 +0900

    글쓰기 기능 구현

--- ThekusDayDiary/ThekusDayDiary/MainView/MainView.swift
+++ ThekusDayDiary/ThekusDayDiary/MainView/MainView.swift
@@ -3,45 +3,67 @@ import UIKit
 class MainView: UIViewController {
     
     @IBOutlet weak var mainTableView: UITableView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
     
-}
-
-extension MainView: UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        tableView.rowHeight = 50
-        return UITableView.automaticDimension
-    }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        mainTableView.deselectRow(at: indexPath, animated: true)
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        UserList.dummyList.count
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MainCell", for: indexPath) as! MainCell
-        let list = UserList.dummyList[indexPath.row]
-        cell.userNickname?.text = list.userNickname
-        cell.profile?.image = list.userProfileImg
-        cell.content?.text = list.content
-        cell.contentImg?.image = resizeImage(image: list.contentImg, width: 280)
-        return cell
-    }
-}
-
-extension MainView {
-    func resizeImage(image: UIImage?, width: CGFloat) -> UIImage? {
-        guard let image = image else {
-            return nil
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            mainTableView.dataSource = self
+            mainTableView.delegate = self
+            
+            // WriteViewController의 delegate 설정
+            if let tabBarController = self.tabBarController,
+               let viewControllers = tabBarController.viewControllers,
+               let writeVC = viewControllers[1] as? WriteViewController {
+                writeVC.delegate = self
+            }
         }
-    let scale = width / image.size.width
-    let height = image.size.height * scale
-    UIGraphicsBeginImageContext(CGSize(width: width, height: height))
-    image.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
-    let newImage = UIGraphicsGetImageFromCurrentImageContext()
-    UIGraphicsEndImageContext()
-    return newImage!
     }
-}
+
+   
+    extension MainView: UITableViewDataSource, UITableViewDelegate {
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            tableView.rowHeight = 50
+            return UITableView.automaticDimension
+        }
+        
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            mainTableView.deselectRow(at: indexPath, animated: true)
+        }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            UserList.dummyList.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MainCell", for: indexPath) as! MainCell
+            let list = UserList.dummyList[indexPath.row]
+            cell.userNickname?.text = list.userNickname
+            cell.profile?.image = list.userProfileImg
+            cell.content?.text = list.content
+            cell.contentImg?.image = resizeImage(image: list.contentImg, width: 280)
+            return cell
+        }
+    }
+
+    
+    extension MainView {
+        func resizeImage(image: UIImage?, width: CGFloat) -> UIImage? {
+            guard let image = image else {
+                return nil
+            }
+            let scale = width / image.size.width
+            let height = image.size.height * scale
+            UIGraphicsBeginImageContext(CGSize(width: width, height: height))
+            image.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+            let newImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            return newImage
+        }
+    }
+
+    
+    extension MainView: WriteViewControllerDelegate {
+        func didCompleteWriting(userList: UserList) {
+            UserList.dummyList.insert(userList, at: 0)
+            mainTableView.reloadData()
+        }
+    }
